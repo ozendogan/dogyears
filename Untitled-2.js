c3e17468a8c33c310bf27673bf541b1df87ca9d0
ozendogan <doganozen2010@gmail.com>
2022-06-02 00:32:02 +0300

    ilk kayit

---
+++ Untitled-2.js
@@ -0,0 +1,15 @@
+const myAge = 10; // benim yaşım 
+let myName = "x".toLowerCase();
+console.log(myName);
+
+let earlyYears = 2;
+earlyYears = earlyYears*=10.5;
+
+let laterYears = myAge-2;
+ laterYears *=4;
+
+let myAgeInDogYears = earlyYears + laterYears;
+
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years ols in dog years.`);
+
